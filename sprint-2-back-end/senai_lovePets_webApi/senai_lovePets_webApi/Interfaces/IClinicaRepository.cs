114e7075c0bc948117c2ec149921326fadaba4b3
Matheus Neri dos Santos <36773915805@info.local>
2021-07-15 17:09:01 -0300

    API até inicio de interface

---
+++ sprint-2-back-end/senai_lovePets_webApi/senai_lovePets_webApi/Interfaces/IClinicaRepository.cs
@@ -0,0 +1,21 @@
+﻿using senai_lovePets_webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_lovePets_webApi.Interfaces
+{
+    interface IClinicaRepository
+    {
+        List<Clinica> ListarTodos();
+
+        Clinica BuscarPorId(int id);
+
+        void Cadastrar(Clinica NovaClinica);
+
+        void Deletar(int id);
+
+        void Atualizar(int id, Clinica NovaClinica);
+    }
+}
